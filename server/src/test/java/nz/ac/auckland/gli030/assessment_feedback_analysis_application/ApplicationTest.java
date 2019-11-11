76edeaefdd06ba1728eb6470e7094fc1bb8c4d26
Gen Li <genli@outlook.co.nz>
2019-11-11 16:18:30 +1300

    Add initial server project files

---
+++ server/src/test/java/nz/ac/auckland/gli030/assessment_feedback_analysis_application/ApplicationTest.java
@@ -0,0 +1,8 @@
+package nz.ac.auckland.gli030.assessment_feedback_analysis_application;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ApplicationTest {
+}
