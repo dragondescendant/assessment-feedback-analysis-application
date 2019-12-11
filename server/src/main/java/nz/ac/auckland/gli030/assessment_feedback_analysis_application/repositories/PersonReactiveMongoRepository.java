c4300f03da33666180107a0f0a7ae44b4b6ff706
Gen Li <genli@outlook.co.nz>
2019-12-12 12:06:21 +1300

    Remove findAllByEmailAddress method from PersonReactiveMongoRepository interface

--- server/src/main/java/nz/ac/auckland/gli030/assessment_feedback_analysis_application/repositories/PersonReactiveMongoRepository.java
+++ server/src/main/java/nz/ac/auckland/gli030/assessment_feedback_analysis_application/repositories/PersonReactiveMongoRepository.java
@@ -1,10 +1,7 @@
 package nz.ac.auckland.gli030.assessment_feedback_analysis_application.repositories;
 
 import nz.ac.auckland.gli030.assessment_feedback_analysis_application.models.Person;
-import org.reactivestreams.Publisher;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Flux;
 
 public interface PersonReactiveMongoRepository extends ReactiveMongoRepository<Person, String> {
-    Flux<Person> findAllByEmailAddress(Publisher<String> emailAddressStream);
 }
